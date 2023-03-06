f6965851683e9b403e2559e1e5ee7cd3e2cc3b62
GoldenMine0502 <ehe123@naver.com>
2023-03-06 14:43:01 +0900

    versions.txt 추가, 키 관리 안해도 될듯?

--- src/main/java/kr/goldenmine/inuminecraftlauncher/login/MicrosoftKeyService.java
+++ src/main/java/kr/goldenmine/inuminecraftlauncher/login/MicrosoftKeyService.java
@@ -1,9 +1,14 @@
 package kr.goldenmine.inuminecraftlauncher.login;
 
+import kr.goldenmine.inuminecraftlauncher.file.FileController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MicrosoftKeyService {
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
     private MicrosoftKey primary;
 
     private final MicrosoftKeyRepository microsoftKeyRepository;
@@ -13,8 +18,12 @@ public class MicrosoftKeyService {
     }
 
     public MicrosoftKey getPrimary() {
-        if(primary == null) {
-            primary = microsoftKeyRepository.findAll().get(0);
+        try {
+            if (primary == null) {
+                primary = microsoftKeyRepository.findAll().get(0);
+            }
+        } catch(Exception ex) {
+            logger.error(ex.getMessage(), ex);
         }
         return primary;
     }
