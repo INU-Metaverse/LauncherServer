81af05e31878ba4a1debe54d7f44b75932e25b27
GoldenMine0502 <ehe123@naver.com>
2023-03-16 23:00:36 +0900

    옵션, 쉐이더 다운로드 (서버)

--- src/main/java/kr/goldenmine/inuminecraftlauncher/file/FileController.java
+++ src/main/java/kr/goldenmine/inuminecraftlauncher/file/FileController.java
@@ -1,5 +1,7 @@
 package kr.goldenmine.inuminecraftlauncher.file;
 
+import kr.goldenmine.inuminecraftlauncher.models.MD5Request;
+import kr.goldenmine.inuminecraftlauncher.util.FileUtilKt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
@@ -17,6 +19,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -83,24 +86,24 @@ public class FileController {
         return getDownloadResource("java/" + os + "/" + fileName, request);
     }
 
+    @GetMapping("/download/versions/{version:.+}")
+    public ResponseEntity<Resource> downloadVersion(@PathVariable String version, HttpServletRequest request) {
+        return getDownloadResource("versions/" + version + ".json", request);
+    }
+
     @GetMapping("/download/mods/{modName:.+}")
     public ResponseEntity<Resource> downloadMod(@PathVariable String modName, HttpServletRequest request) {
         return getDownloadResource("mods/" + modName, request);
     }
 
-    @GetMapping("/download/version/{version:.+}")
-    public ResponseEntity<Resource> downloadVersion(@PathVariable("version") String version, HttpServletRequest request) {
-        return getDownloadResource("versions/" + version  + ".json", request);
-    }
-
     @GetMapping("/download/options/{option:.+}")
-    public ResponseEntity<Resource> downloadOption(@PathVariable("option") String option, HttpServletRequest request) {
-        return getDownloadResource("options/" + option  + ".txt", request);
+    public ResponseEntity<Resource> downloadOption(@PathVariable String option, HttpServletRequest request) {
+        return getDownloadResource("options/" + option + ".txt", request);
     }
 
-    @GetMapping("/download/options/{shader:.+}")
-    public ResponseEntity<Resource> downloadShader(@PathVariable("shader") String shader, HttpServletRequest request) {
-        return getDownloadResource("shaders/" + shader  + ".zip", request);
+    @GetMapping("/download/shaders/{shader:.+}")
+    public ResponseEntity<Resource> downloadShader(@PathVariable String shader, HttpServletRequest request) {
+        return getDownloadResource("shaders/" + shader + ".zip", request);
     }
 
     public ResponseEntity<Resource> getDownloadResource(String fileName, HttpServletRequest request)  {
@@ -126,46 +129,42 @@ public class FileController {
                 .body(resource);
     }
 
-    @GetMapping("/check/java/{fileName:.+}")
-    public ResponseEntity<String> checkFileJava(@PathVariable String fileName) throws Exception {
-        return checkFile("java/" + fileName);
+    @GetMapping("/check/java/{os:.+}/{fileName:.+}")
+    public ResponseEntity<MD5Request> checkFileJava(@PathVariable String os, @PathVariable String fileName) throws Exception {
+        return checkFile("java/" + os + "/" + fileName);
     }
 
-    @GetMapping("/check/version/{fileName:.+}")
-    public ResponseEntity<String> checkFileVersion(@PathVariable String fileName) throws Exception {
-        return checkFile("version/" + fileName);
+    @GetMapping("/check/versions/{version:.+}")
+    public ResponseEntity<MD5Request> checkFileVersion(@PathVariable String version) throws Exception {
+        return checkFile("versions/" + version + ".json");
     }
 
-    @GetMapping("/check/mods/{fileName:.+}")
-    public ResponseEntity<String> checkFileMods(@PathVariable String fileName) throws Exception {
-        return checkFile("mods/" + fileName);
+    @GetMapping("/check/mods/{modName:.+}")
+    public ResponseEntity<MD5Request> checkFileMods(@PathVariable String modName) throws Exception {
+        return checkFile("mods/" + modName);
     }
 
-    @GetMapping("/check/options/{fileName:.+}")
-    public ResponseEntity<String> checkFileOptions(@PathVariable String fileName) throws Exception {
-        return checkFile("options/" + fileName);
+    @GetMapping("/check/options/{option:.+}")
+    public ResponseEntity<MD5Request> checkFileOptions(@PathVariable String option) throws Exception {
+        return checkFile("options/" + option + ".txt");
     }
 
-    @GetMapping("/check/shaders/{fileName:.+}")
-    public ResponseEntity<String> checkFileShaders(@PathVariable String fileName) throws Exception {
-        return checkFile("shaders/" + fileName);
+    @GetMapping("/check/shaders/{shader:.+}")
+    public ResponseEntity<MD5Request> checkFileShaders(@PathVariable String shader) throws Exception {
+        return checkFile("shaders/" + shader + ".zip");
     }
 
-    public ResponseEntity<String> checkFile(String fileName) throws IOException, NoSuchAlgorithmException {
+    private HashMap<String, String> md5Cache = new HashMap<>();
+
+    public ResponseEntity<MD5Request> checkFile(String fileName) throws IOException, NoSuchAlgorithmException {
         Resource resource = fileStorageService.loadFileAsResource(fileName);
 
-        File file = resource.getFile();
-        FileInputStream fileInputStream = new FileInputStream(file);
-        MessageDigest md = MessageDigest.getInstance("MD5");
-
-        byte[] bytes = fileInputStream.readAllBytes();
-        fileInputStream.close();
-
-        md.update(bytes);
-        String hash = Base64.getEncoder().encodeToString(md.digest());
+        if(!md5Cache.containsKey(fileName)) {
+            md5Cache.put(fileName, FileUtilKt.getFileMD5(resource.getFile().getAbsolutePath()));
+        }
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.TEXT_PLAIN);
-        return new ResponseEntity<>(hash, headers, HttpStatus.OK);
+//        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>(new MD5Request(md5Cache.get(fileName)), headers, HttpStatus.OK);
     }
 }
